ab0d407497e54f6d4c0f95cf116455397879b45d
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2017-09-04 16:27:43 -0500

    cron

--- smart-tools/lib/video_converter.rb
+++ smart-tools/lib/video_converter.rb
@@ -13,8 +13,6 @@ def convert_to_mp4(path)
   new_path = converted+ new_path[0,new_path.length-4]
 
   movie.transcode("#{new_path}.mp4", %w(-acodec aac -vcodec h264 -strict -2 -threads 10 -threads 10))
-
-
   move_upload_to_original(path)
 end
 
@@ -28,7 +26,7 @@ def search_files
   inicio = Time.now
   Dir.entries(upload).select {|f| convert_to_mp4(upload+f) unless File.directory?(f)}
   fin = Time.now
-  puts "Miliseconds used to convert video #{(fin-inicio)*1000}"
+  puts "Miliseconds used to convert 1 video #{(fin-inicio)*1000} with 10 threads"
 
   puts "search files"
   # convert_to_mp4(path)
