a785e851268f118f37d52675ad5f323790323e24
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2017-09-04 09:37:40 -0500

    cron

--- smart-tools-sql/lib/video_converter.rb
+++ smart-tools-sql/lib/video_converter.rb
@@ -1,17 +1,23 @@
 require 'streamio-ffmpeg'
 
+def move_upload_to_original(path)
+  new_path = File.basename(path)
+  File.rename(path, "./video/original/#{new_path}")
+end
+
 def convert_to_mp4(path)
   puts "convert to mp4 #{path}"
   movie = FFMPEG::Movie.new(path)
-  options = {strict: "experimental", video_codec:"h264", audio_codec:"aac"}
-  new_path = path[0,path.length-5]
-  movie.transcode("#{new_path}.mp4",options)
+  new_path = File.basename(path)
+  new_path = "./video/converted/"+ new_path[0,new_path.length-4]
+  movie.transcode("#{new_path}.mp4", %w(-acodec aac -vcodec h264 -strict -2))
+  move_upload_to_original(path)
 end
 
 def search_files
-  Dir.entries("./video").select {|f| !File.directory? puts f}
+  #Dir.entries("./video/upload").select {|f| !File.directory? puts f}
   puts "search files"
-  path="./video/Stromae - Ave Cesaria (Live on KEXP).avi"
+  path="./video/upload/Stromae - Ave Cesaria (Live on KEXP).avi"
   convert_to_mp4(path)
 end
 
