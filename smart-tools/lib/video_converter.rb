3dcdb0d1bd9c61161552d827c0ab67843e428c6b
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2017-09-04 16:14:35 -0500

    VideoConverter

--- smart-tools/lib/video_converter.rb
+++ smart-tools/lib/video_converter.rb
@@ -11,7 +11,10 @@ def convert_to_mp4(path)
   converted = "./video/converted/"
   new_path = File.basename(path)
   new_path = converted+ new_path[0,new_path.length-4]
+  inicio = Time.now
   movie.transcode("#{new_path}.mp4", %w(-acodec aac -vcodec h264 -strict -2 -threads 3 -threads 3))
+  fin = Time.now
+  puts "Miliseconds used to convert video #{(fin-inicio)*1000}"
   move_upload_to_original(path)
 end
 
