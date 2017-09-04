c7de966e91b43e96317fc554f079ef9d076a2565
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2017-09-04 08:42:16 -0500

    cron

--- smart-tools-sql/lib/video_converter.rb
+++ smart-tools-sql/lib/video_converter.rb
@@ -5,12 +5,12 @@ def convert_to_mp4(path)
   movie = FFMPEG::Movie.new(path)
   options = { video_codec:"h264", audio_codec:"aac"}
   new_path = path[0,path.length-5]
-  movie.transcode("#{new_path}.mp4",options)
+  movie.transcode("#{new_path}.mp4",%w(-strict experimental -ac aac -vc h264))
 end
 
 def search_files
   puts "search files"
-  path="C:/Users/Q551/Universidad/Cloud/Cloud/smart-tools-sql/video/Stromae - Ave Cesaria (Live on KEXP).avi"
+  path="./video/Stromae - Ave Cesaria (Live on KEXP).avi"
   convert_to_mp4(path)
 end
 
