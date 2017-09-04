c15a4bafb21cab07031ed04bab86b9e22e1ba488
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2017-09-04 10:07:58 -0500

    cron

--- smart-tools-sql/lib/video_converter.rb
+++ smart-tools-sql/lib/video_converter.rb
@@ -8,17 +8,27 @@ end
 def convert_to_mp4(path)
   puts "convert to mp4 #{path}"
   movie = FFMPEG::Movie.new(path)
+  converted = "./video/converted/"
   new_path = File.basename(path)
-  new_path = "./video/converted/"+ new_path[0,new_path.length-4]
-  movie.transcode("#{new_path}.mp4", %w(-acodec aac -vcodec h264 -strict -2))
+  new_path = converted+ new_path[0,new_path.length-4]
+  movie.transcode("#{new_path}.mp4", %w(-acodec aac -vcodec h264 -strict -2 -threads 3))
   move_upload_to_original(path)
 end
 
 def search_files
-  #Dir.entries("./video/upload").select {|f| !File.directory? puts f}
+
+  original = "./video/original"
+  converted = "./video/converted"
+  upload = "./video/upload/"
+  Dir.mkdir(original) unless File.exist?(original)
+  Dir.mkdir(converted) unless File.exist?(converted)
+
+  Dir.entries(upload).select {|f| convert_to_mp4(upload+f) unless File.directory?(f)}
+
+
   puts "search files"
   path="./video/upload/Stromae - Ave Cesaria (Live on KEXP).avi"
-  convert_to_mp4(path)
+  # convert_to_mp4(path)
 end
 
 search_files
